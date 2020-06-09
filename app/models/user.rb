2d66e1517dc29e49c2c1120746e4bd9efbf7db63
kailash-s <kailashnathan@hotmail.co.uk>
2020-06-09 12:55:05 +0100

    working with nested attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  attr_accessible :children_attributes        
   has_many :children
+  accepts_nested_attributes_for :children
 end
