f5f000878f8ec8f2263d3662d27af7d03c730478
kailash-s <kailashnathan@hotmail.co.uk>
2020-06-11 16:37:22 +0100

    added breakdown and faq page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
-    skip_before_action :authenticate_user!, only: :home
+    skip_before_action :authenticate_user!, :only => [:home, :breakdown, :faq]
 
     def home
     end
 
     def thanks
+    end
+
+    def breakdown
+    end
+
+    def faq
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "users", to: "users#show"
   get 'thanks', to: 'pages#thanks'
+  get 'breakdown', to: 'pages#breakdown'
+  get 'faq', to: 'pages#faq'
   
   resources :children
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
