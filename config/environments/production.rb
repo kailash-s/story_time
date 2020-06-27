bc901055acd2212d581f7acb13d1984306e41a58
kailash-s <kailashnathan@hotmail.co.uk>
2020-06-27 21:14:45 +0100

    working to fix email confirmation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'lilypadtales.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
