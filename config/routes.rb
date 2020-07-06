31803107d074d6d4bfdf85806f32cd343319d453
kailash-s <kailashnathan@hotmail.co.uk>
2020-07-06 14:14:25 +0100

    working with contact form

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,6 @@ Rails.application.routes.draw do
   end
   resources :participations
   resources :events
+  resources :contact, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
