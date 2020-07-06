d62d595884dbc1ea3cfefd6fd75c93886e6a8df4
kailash-s <kailashnathan@hotmail.co.uk>
2020-07-06 14:19:11 +0100

    working with contact form

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -21,7 +21,7 @@ Devise.setup do |config|
   config.mailer_sender = 'no-reply@lilypadtales.com'
 
   # Configure the class responsible to send e-mails.
-  config.mailer = 'MailForm::Base'
+  config.mailer = 'Devise::Mailer'
 
   # Configure the parent class responsible to send e-mails.
   config.parent_mailer = 'ActionMailer::Base'
